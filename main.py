dfa0fa77e28184884cf3604062af0d3043914ed3
Jack <59923922+Jacq0@users.noreply.github.com>
2025-03-31 23:54:11 +0100

    Update main.py
    Made drawing functions more generic.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 import random
+import inspect
 
 # pygame setup
 pygame.init()
@@ -37,6 +38,7 @@ def panel(pos, size, colour):
 
     bolt_size = 3
     bolt_offset = 7
+
     #draw bolt details
     pygame.draw.circle(screen, (150,150,150), (pos[0] + bolt_offset, pos[1] + bolt_offset), bolt_size)
     pygame.draw.circle(screen, (150,150,150), (pos[0] + (top_size[0] - bolt_offset), pos[1] + bolt_offset), bolt_size)
@@ -44,37 +46,53 @@ def panel(pos, size, colour):
     pygame.draw.circle(screen, (150,150,150), (pos[0] + (top_size[0] - bolt_offset), pos[1] + (top_size[1] - bolt_offset)), bolt_size)
 
 #will draw shapes wrapped
-def wrap_draw(to_draw, pos, size, dimensions, colour):
-    offset_X = pos[0]
-    offset_Y = pos[1]
+def wrap_draw(to_draw, **kwargs):
+    sig = inspect.signature(to_draw)
+    
+    #check for required args
+    req_kwargs = {kw: i for kw, i in kwargs.items() if kw in sig.parameters}
+
+    #store for later
+    pos_X = req_kwargs["pos"][0] 
+    pos_Y = req_kwargs["pos"][1] 
+    offset_X = req_kwargs["pos"][0] 
+    offset_Y = req_kwargs["pos"][1] 
 
     redraw_X = False
     redraw_Y = False
 
-    if (pos[0] + size[0]) > dimensions[0]:
-        offset_X = (pos[0] - dimensions[0])
+    if (req_kwargs["pos"][0] + req_kwargs["size"][0]) > kwargs["dims"][0]:
+        offset_X = (req_kwargs["pos"][0] - kwargs["dims"][0])
         redraw_X = True
 
-    if (pos[1] + size[1]) > dimensions[1]:
-        offset_Y = (pos[1] - dimensions[1])
+    if (req_kwargs["pos"][1] + req_kwargs["size"][1]) > kwargs["dims"][1]:
+        offset_Y = (req_kwargs["pos"][1] - kwargs["dims"][1])
         redraw_Y = True
 
     #redraw on all axes
     if redraw_X and redraw_Y:
-        print("Redraw all axes")
-        to_draw((pos[0], offset_Y), size, colour)
-        to_draw((offset_X, pos[1]), size, colour)
-        to_draw((offset_X, offset_Y), size, colour)
+        #change kwargs for position (required for wrap drawing)
+        pos_kwarg = (pos_X, offset_Y)
+        req_kwargs["pos"] = pos_kwarg
+        to_draw(**req_kwargs)
 
-    #redraw on X or Y
-    if xor(redraw_X, redraw_Y):
-        print("Redraw X-Y axes")
-        to_draw((offset_X, offset_Y), size, colour)
+        pos_kwarg = (offset_X, pos_Y)
+        req_kwargs["pos"] = pos_kwarg
+        to_draw(**req_kwargs)
 
-#draw a shape from given method, with wrapping
-def draw_shape(to_draw, pos, size, dimensions, colour):
-    to_draw(pos, size, colour)
-    wrap_draw(to_draw, pos, size, dimensions, colour)
+    #kwargs pos is offset
+    pos_kwarg = (offset_X, offset_Y)
+    req_kwargs["pos"] = pos_kwarg
+    to_draw(**req_kwargs) #this draw is done regardless
+
+#draw a shape from given method, with wrapping (initial call plus wrap call)
+def draw_shape(to_draw, **kwargs):
+    sig = inspect.signature(to_draw)
+    
+    req_kwargs = {kw: i for kw, i in kwargs.items() if kw in sig.parameters} #required arguments
+   
+    to_draw(**req_kwargs)
+    wrap_draw(to_draw, pos=kwargs["pos"], size=kwargs["size"], dims=kwargs["dims"], colour=kwargs["colour"])
 
 while running:
     #poll events
@@ -94,7 +112,7 @@ while running:
         darkness = random.randrange(50, 150)
         random_col = (darkness,darkness,darkness)
 
-        draw_shape(panel, random_pos, (random_width, random_height), (width,height), random_col)
+        draw_shape(panel, pos=random_pos, size=(random_width,random_height), dims=(width, height), colour=random_col)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
