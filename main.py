c7a4f8387721b746e3c45faa53868f402939fc73
Jack <59923922+Jacq0@users.noreply.github.com>
2025-04-01 22:09:41 +0100

    Update main.py
    Added new pane draw function and range checks for colours

--- main.py
+++ main.py
@@ -13,37 +13,91 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+def val_in_range(val, min, max):
+    if val > max:
+        val = max
+    elif val < min:
+        val = min
+
+    return val
+
 def xor(bool1, bool2):
     return (bool1 and not bool2) or (not bool1 and bool2)
 
 def rect(pos, size, colour):
     rect = pygame.Rect(pos, size)
+
+    col_array = list(colour)
+    
+    for i, val in enumerate(colour):
+        col_array[i] = val_in_range(val, 0, 255)
+
+    colour = tuple(col_array)
+
     pygame.draw.rect(screen, colour, rect)
 
 def ellipse(pos, size, colour):
     rect = pygame.Rect(pos, size) 
+
+    col_array = list(colour)
+
+    for i, val in enumerate(colour):
+        col_array[i] = val_in_range(val, 0, 255)
+
+    colour = tuple(col_array)
+
     pygame.draw.ellipse(screen, colour, rect)
 
 #draw a panel out of basic shapes
-def panel(pos, size, colour):
+def panel(pos, size, colour, bolt_size=3, bolt_offset=7, top_size=(-1,-1), top_colour=(-1,-1,-1), colour_diff=-15):
+    #set defaults
+    if bolt_size < 0:
+        bolt_size = 3
+
+    #will calculate and set default if user hasn't set any or they're outside spec
+    if top_size[0] < 0 or top_size[1] < 0 or size[0] > 5 or size[1] > 5:
+        top_size = (size[0]-5, size[1]-5)
+
+    #check if colours in range
+    col_array = list(colour)
+    top_col_array = list(top_colour)
+    
+    for i, val in enumerate(colour):
+        col_array[i] = val_in_range(val, 0, 255)
+        top_col_array[i] = val_in_range((val+colour_diff), 0, 255)
+
+    colour = tuple(col_array)
+    top_colour = tuple(top_col_array)
+
     rect = pygame.Rect(pos, size)
-
-    #check these for errors
-    top_size = (size[0]-5, size[1]-5) 
-    top_colour = (colour[0]-15, colour[0]-15, colour[0]-15)
-
     rect_top = pygame.Rect(pos, top_size)
     pygame.draw.rect(screen, colour, rect)
     pygame.draw.rect(screen, top_colour, rect_top)
-
-    bolt_size = 3
-    bolt_offset = 7
 
     #draw bolt details
     pygame.draw.circle(screen, (150,150,150), (pos[0] + bolt_offset, pos[1] + bolt_offset), bolt_size)
     pygame.draw.circle(screen, (150,150,150), (pos[0] + (top_size[0] - bolt_offset), pos[1] + bolt_offset), bolt_size)
     pygame.draw.circle(screen, (150,150,150), (pos[0] + bolt_offset, pos[1] + (top_size[1] - bolt_offset)), bolt_size)
     pygame.draw.circle(screen, (150,150,150), (pos[0] + (top_size[0] - bolt_offset), pos[1] + (top_size[1] - bolt_offset)), bolt_size)
+
+def pane(pos, size, colour, tri_colour=(-1,-1,-1), colour_diff=15):
+    rect = pygame.Rect(pos, size)
+
+    tri_points = (pos, (pos[0] + size[0]-1, pos[1]), (pos[0] ,pos[1] + size[1]-1)) #offset caused by line draw width
+
+    #check if colours in range
+    col_array = list(colour)
+    tri_col_array = list(tri_colour)
+    
+    for i, val in enumerate(colour):
+        col_array[i] = val_in_range(val, 0, 255)
+        tri_col_array[i] = val_in_range((val+colour_diff), 0, 255)
+
+    colour = tuple(col_array)
+    tri_colour = tuple(tri_col_array)
+
+    pygame.draw.rect(screen, colour, rect)
+    pygame.draw.polygon(screen, tri_colour, tri_points)
 
 #will draw shapes wrapped
 def wrap_draw(to_draw, **kwargs):
@@ -80,10 +134,15 @@ def wrap_draw(to_draw, **kwargs):
         req_kwargs["pos"] = pos_kwarg
         to_draw(**req_kwargs)
 
-    #kwargs pos is offset
-    pos_kwarg = (offset_X, offset_Y)
-    req_kwargs["pos"] = pos_kwarg
-    to_draw(**req_kwargs) #this draw is done regardless
+        pos_kwarg = (offset_X, offset_Y)
+        req_kwargs["pos"] = pos_kwarg
+        to_draw(**req_kwargs) 
+
+    if xor(redraw_X, redraw_Y):
+        #kwargs pos is offset
+        pos_kwarg = (offset_X, offset_Y)
+        req_kwargs["pos"] = pos_kwarg
+        to_draw(**req_kwargs) 
 
 #draw a shape from given method, with wrapping (initial call plus wrap call)
 def draw_shape(to_draw, **kwargs):
@@ -92,7 +151,7 @@ def draw_shape(to_draw, **kwargs):
     req_kwargs = {kw: i for kw, i in kwargs.items() if kw in sig.parameters} #required arguments
    
     to_draw(**req_kwargs)
-    wrap_draw(to_draw, pos=kwargs["pos"], size=kwargs["size"], dims=kwargs["dims"], colour=kwargs["colour"])
+    wrap_draw(to_draw, **req_kwargs, dims=kwargs["dims"]) #takes dims args too
 
 while running:
     #poll events
@@ -101,7 +160,7 @@ while running:
             running = False
 
     #fill screen with black to clear
-    screen.fill("black")
+    screen.fill("black") 
 
     for i in range(1000):
         random_col = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
