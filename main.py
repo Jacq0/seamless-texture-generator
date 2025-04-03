d3e875ebae994527869ff6e9b8eda40a8d6da774
Jack <59923922+Jacq0@users.noreply.github.com>
2025-04-03 20:26:25 +0100

    Update main.py
    Moved helper functions to separate script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
 import random
 import inspect
+from helpers import xor, val_in_range
 
 # pygame setup
 pygame.init()
@@ -12,17 +13,6 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-
-def val_in_range(val, min, max):
-    if val > max:
-        val = max
-    elif val < min:
-        val = min
-
-    return val
-
-def xor(bool1, bool2):
-    return (bool1 and not bool2) or (not bool1 and bool2)
 
 def rect(pos, size, colour):
     rect = pygame.Rect(pos, size)
